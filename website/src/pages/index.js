07b5e098d163964a7e8df21d9fd08cbc3c38c08a
Eryn Lynn <eryn@eryn.io>
2021-10-01 20:34:51 -0400

    Fix title and description

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Documentation Generator for Lua Projects"
     >
       <HomepageHeader />
       <main>
