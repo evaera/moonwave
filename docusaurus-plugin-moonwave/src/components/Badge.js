b6acf0f611c438f33f8fa350a1764bda5d3bc4a1
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-10-04 16:09:36 -0500

    Create new wrapper for tooltip tails

--- docusaurus-plugin-moonwave/src/components/Badge.js
+++ docusaurus-plugin-moonwave/src/components/Badge.js
@@ -122,7 +122,10 @@ export default function Badge({ label }) {
         }}
       >
         <span className={clsx(styles.badgeTooltip)}>{title}</span>
-        {image} {label}
+        {/* <div className={clsx(styles.badgeToolTipTail)} /> */}
+        <span className={clsx(styles.badgeToolTipTail)}>
+          {image} {label}
+        </span>
       </span>
     </>
   )
