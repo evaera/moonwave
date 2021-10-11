023e76d13910b97863a1d8990438481f36f49587
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-10-11 16:11:06 -0500

    Trim zero-width spaces from Redirect

--- docusaurus-plugin-moonwave/src/components/Redirect.js
+++ docusaurus-plugin-moonwave/src/components/Redirect.js
@@ -2,10 +2,11 @@ import { Redirect as RouterRedirect } from "@docusaurus/router"
 import React from "react"
 
 export default function Redirect({ allLuaClassNames, pluginOptions }) {
-  const firstLuaClassName =
+  const firstLuaClassName = (
     allLuaClassNames[0].type === "link"
       ? allLuaClassNames[0].label
       : allLuaClassNames[0].items[0].label
+  ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
 
   return (
     <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
