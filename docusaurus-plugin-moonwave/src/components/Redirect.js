6856f8520bde4a05fc830e9a36b223722bdd2d8c
eryn L. K <eryn@eryn.io>
2021-12-25 21:11:01 -0500

    Fix #99

--- docusaurus-plugin-moonwave/src/components/Redirect.js
+++ docusaurus-plugin-moonwave/src/components/Redirect.js
@@ -2,13 +2,17 @@ import { Redirect as RouterRedirect } from "@docusaurus/router"
 import React from "react"
 
 export default function Redirect({ sidebarClassNames, pluginOptions }) {
-  const firstLuaClassName = (
-    sidebarClassNames[0].type === "link"
-      ? sidebarClassNames[0].label
-      : sidebarClassNames[0].items[0].label
-  ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
+  if (sidebarClassNames.length > 0) {
+    const firstLuaClassName = (
+      sidebarClassNames[0].type === "link"
+        ? sidebarClassNames[0].label
+        : sidebarClassNames[0].items[0].label
+    ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
 
-  return (
-    <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
-  )
+    return (
+      <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
+    )
+  } else {
+    return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
+  }
 }
