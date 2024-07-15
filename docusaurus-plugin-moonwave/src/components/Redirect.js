be6d501223472300c45ac1b6fd675f247f23dbf4
Clownxz <Fatestheadgamin@gmail.com>
2024-07-15 01:05:25 -0400

    Fix API page crash with classOrder (#137)

--- docusaurus-plugin-moonwave/src/components/Redirect.js
+++ docusaurus-plugin-moonwave/src/components/Redirect.js
@@ -3,16 +3,23 @@ import React from "react"
 
 export default function Redirect({ sidebarClassNames, pluginOptions }) {
   if (sidebarClassNames.length > 0) {
-    const firstLuaClassName = (
-      sidebarClassNames[0].type === "link"
-        ? sidebarClassNames[0].label
-        : sidebarClassNames[0].items[0].label
-    ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
+    for (let index = 0; index < sidebarClassNames.length; index++) {
+      const element = sidebarClassNames[index]
+      if (element.type == "category" && element.items.length === 0) {
+        continue
+      }
 
-    return (
-      <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
-    )
-  } else {
-    return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
+      const firstLuaClassName = (
+        element.type === "link" ? element.label : element.items[0].label
+      ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
+
+      return (
+        <RouterRedirect
+          to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`}
+        />
+      )
+    }
   }
+
+  return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
 }
