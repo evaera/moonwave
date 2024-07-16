b12b20236ea1882097fbdcf9b4bf445d9aab3ad4
Clown <yetanotherclown@proton.me>
2024-07-16 05:28:14 -0400

    Fix API page crash with `classOrder` with empty `classes` table (#148)
    * Fix API page crash with classOrder (#137)

--- docusaurus-plugin-moonwave/src/components/Redirect.js
+++ docusaurus-plugin-moonwave/src/components/Redirect.js
@@ -2,17 +2,20 @@ import { Redirect as RouterRedirect } from "@docusaurus/router"
 import React from "react"
 
 export default function Redirect({ sidebarClassNames, pluginOptions }) {
-  if (sidebarClassNames.length > 0) {
+  for (let index = 0; index < sidebarClassNames.length; index++) {
+    const element = sidebarClassNames[index]
+    if (element.type == "category" && element.items.length === 0) {
+      continue
+    }
+
     const firstLuaClassName = (
-      sidebarClassNames[0].type === "link"
-        ? sidebarClassNames[0].label
-        : sidebarClassNames[0].items[0].label
+      element.type === "link" ? element.label : element.items[0].label
     ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
 
     return (
       <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
     )
-  } else {
-    return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
   }
+
+  return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
 }
