0483f56c5c5f987e974f88555f0240636f447a83
Clownxz <Fatestheadgamin@gmail.com>
2024-07-16 05:26:18 -0400

    Remove now redundant if-statement

--- docusaurus-plugin-moonwave/src/components/Redirect.js
+++ docusaurus-plugin-moonwave/src/components/Redirect.js
@@ -2,23 +2,19 @@ import { Redirect as RouterRedirect } from "@docusaurus/router"
 import React from "react"
 
 export default function Redirect({ sidebarClassNames, pluginOptions }) {
-  if (sidebarClassNames.length > 0) {
-    for (let index = 0; index < sidebarClassNames.length; index++) {
-      const element = sidebarClassNames[index]
-      if (element.type == "category" && element.items.length === 0) {
-        continue
-      }
-
-      const firstLuaClassName = (
-        element.type === "link" ? element.label : element.items[0].label
-      ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
-
-      return (
-        <RouterRedirect
-          to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`}
-        />
-      )
+  for (let index = 0; index < sidebarClassNames.length; index++) {
+    const element = sidebarClassNames[index]
+    if (element.type == "category" && element.items.length === 0) {
+      continue
     }
+
+    const firstLuaClassName = (
+      element.type === "link" ? element.label : element.items[0].label
+    ).replace(/[\u200B]/g, "") // Strip out any extraneous 0-width spaces
+
+    return (
+      <RouterRedirect to={`${pluginOptions.baseUrl}api/${firstLuaClassName}`} />
+    )
   }
 
   return <RouterRedirect to={`${pluginOptions.baseUrl}api/404`} />
